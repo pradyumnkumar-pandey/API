2f0abcbd08a815c456c7ba2970299d4b4d29ea48
Pradyumn Kumar Pandey <110444601+pradyumnkumar-pandey@users.noreply.github.com>
2022-09-13 18:36:21 +0530

    Delete blog_apiview directory

--- blog_apiview/role_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# Create your models here.
-
-
-class Role(models.Model):
-    role_user=models.IntegerField()
-    name=models.CharField(null=True,blank=True,max_length=10)
-    def __str__(self):
-        return self.name
